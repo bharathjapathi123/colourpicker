50312bf302bd4429d7e96e6f5fa5d456bb29c190
bharathjapathi123 <japathibharath3@gmail.com>
2026-04-29 23:35:45 +0530

    Add files via upload

---
+++ colourpicker.js
@@ -0,0 +1,22 @@
+let container_background = document.getElementById("colorPickerContainer");
+let colorCode = document.getElementById("selectedColorHexCode")
+
+function button1() {
+    container_background.style.backgroundColor = "#e0e0e0";
+    colorCode.textContent = "#e0e0e0";
+}
+
+function button2() {
+    container_background.style.backgroundColor = "#6fcf97";
+    colorCode.textContent = "#6fcf97";
+}
+
+function button3() {
+    container_background.style.backgroundColor = "#56ccf2";
+    colorCode.textContent = "#56ccf2";
+}
+
+function button4() {
+    container_background.style.backgroundColor = "#bb6bd9";
+    colorCode.textContent = "#bb6bd9";
+}
